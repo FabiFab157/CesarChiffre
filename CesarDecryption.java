295eb148664e8c5a83a6f2f9e53a7ddb3d07e6da
mrX <157production@mailbox.org>
2022-05-19 22:42:54 +0200

    inputReader CesarDycription
    added a input Reader for CesarDycription

--- CesarDecryption.java
+++ CesarDecryption.java
@@ -5,10 +5,13 @@ import java.util.List;
 //import java.util.List;
 
 public class CesarDecryption {
+    public static char[] abcArray = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    public static char[] pwArray1;
     public static void main(String[] args) {
-        char[] abcArray = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        char[] pwArray = {'m','h','i','p',};
-        StringBuilder pwDecrypted = new StringBuilder();
+        
+        inputReader();
+        //char[] pwArray = {'g','b','c','j',}; 
+        StringBuilder pwDecrypted = new StringBuilder(); //StringBuilder helps to build a string letter by letter
         int key = 0;
         int positionABC = 0;
         List<String> pwDecryptedList = new ArrayList<String>();
@@ -17,9 +20,9 @@ public class CesarDecryption {
        
         while(key < 26)
         {
-            for(int i = 0; i < pwArray.length; i++)
+            for(int i = 0; i < pwArray1.length; i++)
             {
-                char pwBuchstabe = pwArray[i];
+                char pwBuchstabe = pwArray1[i];
                 for(int j = 0; j < abcArray.length; j++)
                 {               
                     char abcBuchstabe = abcArray[j];
@@ -41,7 +44,7 @@ public class CesarDecryption {
             key++;
         }
         
-        System.out.println("Passwort Länge " + pwArray.length);
+        System.out.println("Passwort Länge " + pwArray1.length);
         int count = 0;
         for (String string : pwDecryptedList) {
             System.out.println("Nr. " + count + ": " + string);
@@ -49,6 +52,17 @@ public class CesarDecryption {
         }
         System.out.println("------Programm End------");
         
+    }
+
+    //method to read in the encrypted text 
+    public static char[] inputReader(){
+        System.out.println("Type in your encrypted text");
+        String reader = System.console().readLine();
+        reader.replace(" ", "");
+        String input = reader.toLowerCase();
+        pwArray1 = input.toCharArray();
+        
+        return pwArray1;
     }
 }
                 
