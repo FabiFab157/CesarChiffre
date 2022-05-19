2d3d1290db3f173bbd0f4debaa2410784122503d
mrX <157production@mailbox.org>
2022-05-19 23:03:11 +0200

    CesarDecription sorted
    CesarDecription.java file has been cleaned up

--- CesarDecryption.java
+++ CesarDecryption.java
@@ -5,19 +5,37 @@ import java.util.List;
 //import java.util.List;
 
 public class CesarDecryption {
-    public static char[] abcArray = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    public static char[] pwArray1;
+    private static char[] abcArray = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    private static char[] pwArray1;
+    private static List<String> pwDecryptedList = new ArrayList<String>();
+    private static int key = 0;
+    private static int positionABC = 0;
     public static void main(String[] args) {
-        
-        inputReader();
-        //char[] pwArray = {'g','b','c','j',}; 
-        StringBuilder pwDecrypted = new StringBuilder(); //StringBuilder helps to build a string letter by letter
-        int key = 0;
-        int positionABC = 0;
-        List<String> pwDecryptedList = new ArrayList<String>();
-        
         System.out.println("------Program Start------");
-       
+        
+        decrypt(inputReader());
+        decryptList(pwDecryptedList);
+
+        
+        System.out.println("------Programm End------");
+        
+    }
+
+    //method to read in the encrypted text 
+    public static char[] inputReader(){
+        System.out.println("Type in your encrypted text");
+        String reader = System.console().readLine();
+        reader.replace(" ", "");
+        String input = reader.toLowerCase();
+        pwArray1 = input.toCharArray();
+        
+        return pwArray1;
+    }
+
+    //method to decrypt text
+    public static void decrypt(char[] encryptedText){
+         //StringBuilder helps to build a string letter by letter
+         StringBuilder pwDecrypted = new StringBuilder(); 
         while(key < 26)
         {
             for(int i = 0; i < pwArray1.length; i++)
@@ -43,26 +61,17 @@ public class CesarDecryption {
             pwDecrypted.setLength(0);
             key++;
         }
-        
+
+    }
+    
+    //print decrypted List on consol
+    public static void decryptList(List<String>list){
         System.out.println("Passwort Länge " + pwArray1.length);
         int count = 0;
         for (String string : pwDecryptedList) {
             System.out.println("Nr. " + count + ": " + string);
             count++;
         }
-        System.out.println("------Programm End------");
-        
-    }
-
-    //method to read in the encrypted text 
-    public static char[] inputReader(){
-        System.out.println("Type in your encrypted text");
-        String reader = System.console().readLine();
-        reader.replace(" ", "");
-        String input = reader.toLowerCase();
-        pwArray1 = input.toCharArray();
-        
-        return pwArray1;
     }
 }
                 
