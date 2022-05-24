f516592d505476a2b295d9db0f78e616680b270c
mrX <157production@mailbox.org>
2022-05-24 16:32:12 +0200

    typed in message check
    now the inputReader is checking of only letters has been typed, if not program will break up

--- CesarEncryption.java
+++ CesarEncryption.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+
 /**
  * Cesar Entschlüsselung
  */
@@ -16,67 +18,90 @@ public class CesarEncryption {
 
     //Method to read a String in and spliting it to parts
     private static char[] inputReader(){
-        //Read letters in an split them in parts 
-        System.out.println("Type in your message");
-        Scanner readIn = new Scanner(System.in);
-        
-        
-            if(readIn.hasNextDouble()){
-                System.out.println("Please type in only letters");
-                System.out.println("You have typed in: " + readIn.nextInt());
-            } else {
-                String reader = readIn.toString();
-                reader.replace(" ", "");
-                reader = reader.toLowerCase();
-                pwArray = reader.toCharArray();
-            }
-            readIn.close();
-        
-        /* String reader = System.console().readLine();  
-        reader.replace(" ", "");    
-        String input = reader.toLowerCase();
-        pwArray = input.toCharArray(); */
+        //variable to stop loop when input of user is a number or special character
+        boolean goOn = true;
+        while(goOn == true){
+               //Read letters in an split them in parts
+                System.out.println("Type in your message");
+                String reader = System.console().readLine();  
+                reader.replace(" ", "");    
+                String input = reader.toLowerCase();
 
-        return pwArray;        
+                pwArray = input.toCharArray();
+                if(isNumer(pwArray) == true){
+                    goOn = false;
+                    return pwArray = null;   
+                }
+                goOn = false; //stop the loop
+        }
+        return pwArray;
     }
 
     private static int encryptKey()
     {
-        //request for encryotion key
-        Scanner keyInput = new Scanner(System.in);
-        System.out.println("Type in your key");
-        int key = keyInput.nextInt();
-        keyInput.close();
-        return key;
+        int key;
+        if(pwArray == null)
+        {
+            return key = 0;
+            
+        }else{
+            //request for encryotion key
+            Scanner keyInput = new Scanner(System.in);
+            System.out.println("Type in your key");
+            key = keyInput.nextInt();
+            keyInput.close();
+            return key;
+        }         
     }
 
     //Mehtod to encrypt message
     private static void encrypt(char[] input, int schluessel){
-        //Separate ABC string by character and convert to CharArray
-        String str = strAbc.replace(",", "");
-        char[] abcChar = str.toCharArray();
-        char[] encryptText = new char[input.length];
-        int postion = 0;
+        if(pwArray != null)
+        {
+            //Separate ABC string by character and convert to CharArray
+            String str = strAbc.replace(",", "");
+            char[] abcChar = str.toCharArray();
+            char[] encryptText = new char[input.length];
+            int postion = 0;
 
-        //added method with modulu
-        for(int i = 0; i < input.length; i++){
-            for(int j = 0; j < abcChar.length; j++){
-                if(input[i] == abcChar[j]){
-                    postion = j + schluessel;
-                    if(postion > abcChar.length-1)
-                    {
-                        postion = postion % abcChar.length;
+            //added method with modulu
+            for(int i = 0; i < input.length; i++){
+                for(int j = 0; j < abcChar.length; j++){
+                    if(input[i] == abcChar[j]){
+                        postion = j + schluessel;
+                        if(postion > abcChar.length-1)
+                        {
+                            postion = postion % abcChar.length;
+                        }
+                        encryptText[i] = abcChar[postion];
                     }
-                    encryptText[i] = abcChar[postion];
+                }
+            }
+            System.out.print("Encrypted message: ");
+            System.out.println(encryptText);  
+        }else{
+            System.out.println("Program is closed");
+        }  
+    }
+
+    //Methode to verify if a number or char has been typed in
+    private static boolean isNumer(char[] inputArray)
+    {
+        char[] wrongTypes = {'1','2','3','4','5','6','7','8','9','.',',',';','!'};
+        for(int i = 0; i < inputArray.length; i++){
+            for(int j = 0; j < wrongTypes.length; j++){
+                if(inputArray[i] == wrongTypes[j]){
+                    System.out.println("WRONG TYPING number or special character ARE NOT ALLOWED!");
+                    System.out.println("You have typed: " +  inputArray[i]);
+                    return true;
                 }
             }
         }
-        System.out.print("Encrypted message: ");
-        System.out.println(encryptText);   
+        return false;
     }
+    
 }
 
 /*Thigs to do
 - erro catch type in message --> numbers check
-- erro catch type in key --> letter check
 */
